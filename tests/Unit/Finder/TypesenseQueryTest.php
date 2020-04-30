54923e0fdb3910b1fd4fddbc1320206287503837
Nicolas Potier <nicolas.potier@acseo-conseil.fr>
2020-04-30 21:53:14 +0200

    begin tests

---
+++ tests/Unit/Finder/TypesenseQueryTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ACSEO\Bundle\TypesenseBundle\Tests\Unit\Finder;
+
+use ACSEO\TypesenseBundle\Finder\TypesenseQuery;
+use PHPUnit\Framework\TestCase;
+
+class TypesenseQueryTest extends TestCase
+{
+
+    public function testSimpleQuery()
+    {
+        $query = new TypesenseQuery('search term', 'search field');
+
+        $this->assertEquals(
+            ['q' => 'search term', 'query_by' => 'search field'], 
+            $query->getParameters()
+        );
+    }
+
+    public function testComplexQuery()
+    {
+        $query = (new TypesenseQuery('search term', 'search field'))
+            ->prefix(false)
+            ->filterBy('filter term')
+            ->sortBy('sort term')
+            ->facetBy('facet term')
+            ->maxFacetValues(10)
+            ->numTypos(0)
+            ->page(1)
+            ->perPage(20)
+            ->includeFields('field1,field2')
+            ->excludeFields('field3,field4')
+            ->dropTokensThreshold(0)
+        ;
+        
+        $this->assertEquals(
+            [
+                'q' => 'search term', 
+                'query_by' => 'search field',
+                'prefix' => false,
+                'filter_by' => 'filter term',
+                'sort_by' =>'sort term',
+                'facet_by' => 'facet term',
+                'max_facet_values' => 10,
+                'num_typos' => 0,
+                'page' => 1,
+                'per_page' => 20,
+                'include_fields' =>'field1,field2',
+                'exclude_fields' => 'field3,field4',
+                'drop_tokens_threshold' => 0
+            ], 
+            $query->getParameters()
+        );
+    }
+}
