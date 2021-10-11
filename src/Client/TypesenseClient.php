cc7a596961703c7f39f9a05c718d10c8e6d1ca11
Pierre PIRNAY <Pierre.PIRNAY@cigest-group.fr>
2021-10-11 15:50:24 +0200

    Allow to use of null for testing mode
    See https://github.com/acseo/TypesenseBundle/issues/8 for which the fix was reverted with https://github.com/acseo/TypesenseBundle/issues/23

--- src/Client/TypesenseClient.php
+++ src/Client/TypesenseClient.php
@@ -2,24 +2,124 @@
 
 namespace ACSEO\TypesenseBundle\Client;
 
+use Typesense\Aliases;
 use Typesense\Client;
+use Typesense\Collections;
+use Typesense\Debug;
+use Typesense\Health;
+use Typesense\Keys;
+use Typesense\Metrics;
+use Typesense\MultiSearch;
+use Typesense\Operations;
 
-class TypesenseClient extends Client
+class TypesenseClient
 {
+    private ?Client $client = null;
+
     public function __construct(string $url, string $apiKey)
     {
+        if ($url === 'null') {
+            return;
+        }
+
         $urlParsed = parse_url($url);
-        
-        parent::__construct([
-            'nodes'        => [
+
+        $this->client = new Client([
+            'nodes'                      => [
                 [
                     'host'     => $urlParsed['host'],
                     'port'     => $urlParsed['port'],
                     'protocol' => $urlParsed['scheme'],
                 ],
             ],
-            'api_key'      => $apiKey,
+            'api_key'                    => $apiKey,
             'connection_timeout_seconds' => 5,
         ]);
     }
+
+    public function getCollections(): ?Collections
+    {
+        if (!$this->client) {
+            return null;
+        }
+
+        return $this->client->collections;
+    }
+
+    public function getAliases(): ?Aliases
+    {
+        if (!$this->client) {
+            return null;
+        }
+
+        return $this->client->aliases;
+    }
+
+    public function getKeys(): ?Keys
+    {
+        if (!$this->client) {
+            return null;
+        }
+
+        return $this->client->keys;
+    }
+
+    public function getDebug(): ?Debug
+    {
+        if (!$this->client) {
+            return null;
+        }
+
+        return $this->client->debug;
+    }
+
+    public function getMetrics(): ?Metrics
+    {
+        if (!$this->client) {
+            return null;
+        }
+
+        return $this->client->metrics;
+    }
+
+    public function getHealth(): ?Health
+    {
+        if (!$this->client) {
+            return null;
+        }
+
+        return $this->client->health;
+    }
+
+    public function getOperations(): ?Operations
+    {
+        if (!$this->client) {
+            return null;
+        }
+
+        return $this->client->operations;
+    }
+
+    public function getMultiSearch(): ?MultiSearch
+    {
+        if (!$this->client) {
+            return null;
+        }
+
+        return $this->client->multiSearch;
+    }
+
+    /**
+     * This allow to be use to use new Typesense\Client functions
+     * before we update this client.
+     */
+    public function __call($name, $arguments)
+    {
+        if (!$this->client) {
+            return null;
+        }
+
+        return $this->client->$name(...$arguments);
+    }
+
 }
