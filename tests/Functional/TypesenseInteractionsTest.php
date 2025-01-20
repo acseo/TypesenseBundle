36fb9e1b400c5f1edcfc3d45986069a1ce3ae0e5
Nicolas Potier <nicolas.potier@acseo-conseil.fr>
2025-01-20 23:49:24 +0100

    update doctrine and dependencies

--- tests/Functional/TypesenseInteractionsTest.php
+++ tests/Functional/TypesenseInteractionsTest.php
@@ -20,7 +20,7 @@ use Doctrine\DBAL\Connection;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
